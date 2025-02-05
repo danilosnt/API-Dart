557916691d3c22acabd6caf51b8cf93ebce8059a
danilosnt <daniloddocarmo16@icloud.com>
2025-02-05 17:45:42 -0300

    criação do método "convertBytes"

---
+++ api/server.dart
@@ -0,0 +1,27 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'dart:io';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+Response convertBytes(Request request) async {
+  try {
+    final queryParams = request.url.queryParameters;
+    if (!queryParams.containsKey('bytes')) {
+      return Response.badRequest(body: jsonEncode({'error': 'Parâmetro "bytes" não informado'}));
+    }
+
+    final int bytes = int.parse(queryParams['bytes']);
+
+    final conversion = {
+      'KB': bytes / 1024,
+      'MB': bytes / 1024 / 1024,
+      'GB': bytes / 1024 / 1024 / 1024,
+      'TB': bytes / 1024 / 1024 / 1024 / 1024,
+    };
+    
+    return Response.ok(jsonEncode(conversion), headers: {'Content-Type': 'application/json'});
+  } catch (e) {
+    return Response.internalServerError(body: jsonEncode({'error': e.toString()}));
+  }
+}
