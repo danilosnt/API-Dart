4b836b47dff50ab3191ea28802fce365170702d7
danilosnt <daniloddocarmo16@icloud.com>
2025-02-06 21:14:39 -0300

    translate client to english

--- client/client.dart
+++ client/client.dart
@@ -4,40 +4,39 @@ import 'package:http/http.dart' as http;
 
 Future<void> convertBytes(int bytes) async {
   final url = Uri.parse('http://localhost:8080?bytes=$bytes');
-
   final response = await http.get(url);
 
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
-    print('Conversão de $bytes bytes:');
+    print('Conversion of $bytes bytes:');
     print('KB: ${data["KB"].toStringAsFixed(2)}');
     print('MB: ${data["MB"].toStringAsFixed(2)}');
     print('GB: ${data["GB"].toStringAsFixed(2)}');
     print('TB: ${data["TB"].toStringAsFixed(2)}');
   } else {
-    print('Erro ao converter bytes: ${response.body}');
+    print('Error converting bytes: ${response.body}');
   }
 }
 
 void main() async {
   while (true) {
     
-    print('\nDigite o número de bytes para conversão:');
+    print('\nEnter the number of bytes to convert:');
     String? input = stdin.readLineSync();
 
     if (input != null && int.tryParse(input) != null) {
       int bytes = int.parse(input);
       await convertBytes(bytes);
     } else {
-      print('Entrada inválida. Por favor, insira um número inteiro válido.');
+      print('Invalid input. Please enter a valid integer.');
       continue;
     }
 
-    print('\nDeseja converter outro valor? \n Digite "yes" para continuar ou qualquer outra tecla para sair.');
-    String? resposta = stdin.readLineSync()?.trim().toLowerCase();
+    print('\nDo you want to convert another value? \n Type "yes" to continue or any other key to exit.');
+    String? response = stdin.readLineSync()?.trim().toLowerCase();
 
-    if (resposta != 'yes') {
-      print('Encerrando o programa...');
+    if (response != 'yes') {
+      print('Exiting the program...');
       break;
     }
   }
